8a34ff28d63de67838abdabd71606ebc9847b8ac
John DeRiggi <john.deriggi@gmail.com>
2019-03-27 03:13:07 -0400

    overlay

--- src/index.js
+++ src/index.js
@@ -18,7 +18,10 @@ const Index = () => {
 //   </div>
 // </div>;
 
-return <div className="w-full max-w-xs bg-white mx-auto mt-5 shadow-md" >
+return  <div class = "pin absolute flex items-center justify-center  bg-overlay" >
+ 
+ <div className="w-full max-w-xs bg-white mx-auto mt-5 shadow-md p-4" >
+        
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/3">
             <label className="block text-grey font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
@@ -26,7 +29,7 @@ return <div className="w-full max-w-xs bg-white mx-auto mt-5 shadow-md" >
             </label>
           </div>
           <div className="md:w-2/3">
-            <input className="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="inline-full-name" type="text" defaultValue="Jane Doe"></input>
+            <input className="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="inline-full-name" type="text" placeholder="Jane Doe" ></input>
           </div>
         </div>
         <div className="md:flex md:items-center mb-6">
@@ -36,11 +39,13 @@ return <div className="w-full max-w-xs bg-white mx-auto mt-5 shadow-md" >
             </label>
           </div>
           <div className="md:w-2/3">
-            <input className="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="inline-full-name" type="text" defaultValue="password"></input>
+            <input className="bg-grey-lighter appearance-none border-2 border-grey-lighter rounded w-full py-2 px-4 text-grey-darker leading-tight focus:outline-none focus:bg-white focus:border-purple" id="inline-full-name" type="text" placeholder="password"></input>
           </div>
         </div>
-      </div>;
+        </div> 
+        </div>;
   
+
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
